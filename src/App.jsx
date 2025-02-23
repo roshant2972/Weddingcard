0f069a62ad5d052331630d903ef3d86834e5a0c0
Roshan Tirkey <roshan@Roshans-MacBook-Air.local>
2025-02-23 13:35:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function App(){
+   
+  return(
+    <>
+     <div className="outer">
+     
+    
+     
+      <div className='inner'>
+      <img className= "upper_flower sm-12 " src="upper_flower.png" alt="no image" />
+       
+     <img src="down_flower.png" alt="down_flower.png" className="down_flower sm-12" />
+     
+        <div className="inner2">
+        <div className='upper_text '>Uniting Two  Hearts</div>
+        <div className="middle_text">Emma <br /> & <br /> Jack</div>
+        <div className="lower_text">You are cordially invited to celebrate the
+ union of [Bride's Name] and [Groom's Name] as they
+ exchange vows and begin their journey of love and togetherness."</div>
+        </div>
+        <h1></h1>
+      </div>
+      
+
+     </div>
+    </>
+  )
+}
+export default App;
